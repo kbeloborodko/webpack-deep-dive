d856128191ffc6c801595de16f26e9992bcbf603
kbeloborodko <kbeloborodko@gmail.com>
2017-01-19 17:06:15 +0200

    Add first working config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,9 @@
+const {resolve} = require('path');
+
 module.exports = () => {
   return {
+    context: resolve('src'),
+    entry: './bootstrap.js',
     output: {
       filename: 'bundle.js'
     }
