0d529888ab91c9681f3b18496f3ecd39e8c715c1
kbeloborodko <kbeloborodko@gmail.com>
2017-01-20 17:30:56 +0200

    Add hot module replacement

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,26 @@
+/* eslint no-console:0 */
 var app = require('./app')
 var helpers = require('./helpers')
 
-helpers.$on(window, 'load', app.onLoad)
-helpers.$on(window, 'hashchange', app.onLoad)
+// this is only relevant when using `hot` mode with webpack
+const reloading = document.readyState === 'complete'
+if (module.hot) {
+  module.hot.accept(function(err) {
+    console.log('❌  HMR Error:', err)
+  })
+  if (reloading) {
+    console.log('🔁  HMR Reloading.')
+    app.onLoad()
+  } else {
+    console.info('✅  HMR Enabled.')
+    bootstrap()
+  }
+} else {
+  console.info('❌  HMR Not Supported.')
+  bootstrap()
+}
+
+function bootstrap() {
+  helpers.$on(window, 'load', app.onLoad)
+  helpers.$on(window, 'hashchange', app.onLoad)
+}
