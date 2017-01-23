49bf851f4641e94c1719cf301adda6db8e4d4dbf
kbeloborodko <kbeloborodko@gmail.com>
2017-01-23 17:19:36 +0200

    Add tree shaking

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-export {qs, qsa, log, $on, $delegate, $parent, remove, leftPad}
+export {qs, qsa, log, $on, $delegate, $parent, remove}
 
 // Get element(s) by CSS selector:
 function qs(selector, scope) {
@@ -60,16 +60,6 @@ function remove(array, thing) {
     return array
   }
   array.splice(index, 1)
-}
-
-// pad the left of the given string by the given size with the given character
-// leftPad('10', 3, '0') -> 010
-function leftPad(str, size, padWith) {
-  if (size <= str.length) {
-    return str
-  } else {
-    return Array(size - str.length + 1).join(padWith || '0') + str
-  }
 }
 
 // Allow for looping on nodes by chaining:
